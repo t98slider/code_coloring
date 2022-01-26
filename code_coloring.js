b4c82512e69098f8bb75285612130b499026dbe1
t98slider <72385984+t98slider@users.noreply.github.com>
2022-01-26 14:03:28 +0900

    Add files via upload

---
+++ code_coloring.js
@@ -0,0 +1,11 @@
+document.write(`
+<link rel='stylesheet' id='hcb-style-css'  href='CSS_JS_internal/hcb_style.css' type='text/css' media='all' />
+<link rel='stylesheet' id='hcb-coloring-css'  href='CSS_JS_internal/coloring_dark.css' type='text/css' media='all' />
+<style id='hcb-style-inline-css' type='text/css'>
+.hcb_wrap pre.prism{font-size: 14px}@media screen and (max-width: 599px){.hcb_wrap pre.prism{font-size: 13px}}.hcb_wrap pre{font-family:Menlo, Consolas, "メイリオ", sans-serif;}
+</style>
+<script type='text/javascript' src='CSS_JS_internal/clipboard.js' id='clipboard-js'></script>
+<script type='text/javascript' src='CSS_JS_internal/prism.js' id='hcb-prism-js'></script>
+<script type='text/javascript' src='CSS_JS_internal/hcb_script.js' id='hcb-script-js'></script>
+<script type='text/javascript' id='hcb-script-js-extra'>var hcbVars = {"showCopy":"on"};</script>
+`);
